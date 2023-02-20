4ca005100e24f256c19ac063fbddf4081e936e6a
Praveen Koka <praveenkoka@gmail.com>
2023-02-20 11:11:00 +0100

    dashboard page

--- nextjs-with-roq-auth/src/pages/dashboard/index.tsx
+++ nextjs-with-roq-auth/src/pages/dashboard/index.tsx
@@ -1,7 +1,4 @@
 import AppLayout from "layout/app/app.layout";
-import Files from "components/file/files";
-import { requireNextAuth } from "@roq/nextjs";
-import styles from "pages/dashboard/dashboard.module.css";
 
 function DashboardPage() {
   return <AppLayout>Dashboard</AppLayout>;
