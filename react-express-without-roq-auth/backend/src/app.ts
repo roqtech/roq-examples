afb02c654c845b98371ede436cf5ab47d1a60954
Ahmed Mahmoud <ahmed.mahmoud.abo@gmail.com>
2023-02-23 15:31:37 +0200

    added roq auth

--- react-express-without-roq-auth/backend/src/app.ts
+++ react-express-without-roq-auth/backend/src/app.ts
@@ -5,12 +5,11 @@ import express from 'express';
 import helmet from 'helmet';
 import hpp from 'hpp';
 import morgan from 'morgan';
-import swaggerJSDoc from 'swagger-jsdoc';
-import swaggerUi from 'swagger-ui-express';
 import { NODE_ENV, PORT, LOG_FORMAT, ORIGIN, CREDENTIALS } from '@config';
 import { Routes } from '@interfaces/routes.interface';
 import errorMiddleware from '@middlewares/error.middleware';
 import { logger, stream } from '@utils/logger';
+import { auth } from '@roq/expressjs';
 
 class App {
   public app: express.Application;
@@ -24,7 +23,6 @@ class App {
 
     this.initializeMiddlewares();
     this.initializeRoutes(routes);
-    this.initializeSwagger();
     this.initializeErrorHandling();
   }
 
@@ -56,22 +54,7 @@ class App {
     routes.forEach(route => {
       this.app.use('/', route.router);
     });
-  }
-
-  private initializeSwagger() {
-    const options = {
-      swaggerDefinition: {
-        info: {
-          title: 'REST API',
-          version: '1.0.0',
-          description: 'Example docs',
-        },
-      },
-      apis: ['swagger.yaml'],
-    };
-
-    const specs = swaggerJSDoc(options);
-    this.app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+    this.app.use(auth());
   }
 
   private initializeErrorHandling() {
