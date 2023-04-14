804f7b7ca2a2579a4d8efe45e32f15c2ec4f3062
mohsin <mohsinamjad51@gmail.com>
2023-04-14 15:22:35 +0500

    CPD-2756: chat conversation redirect fix

--- nextjs-with-roq-auth/src/pages/chat/fetch-conversation.tsx
+++ nextjs-with-roq-auth/src/pages/chat/fetch-conversation.tsx
@@ -1,47 +1,66 @@
-import React, { useState } from 'react';
-import AppLayout from 'layout/app/app.layout';
-import DemoLayout from 'layout/demo/demo.layout';
-import AuthLayout from 'layout/auth/auth.layout';
-import { routes } from '../../routes';
-import { toast } from 'react-hot-toast';
+import React, { useCallback, useState } from "react";
+import AppLayout from "layout/app/app.layout";
+import DemoLayout from "layout/demo/demo.layout";
+import AuthLayout from "layout/auth/auth.layout";
+import { routes } from "../../routes";
+import { toast } from "react-hot-toast";
+import { useRouter } from "next/router";
 
 const FetchConversation = () => {
-    const [conversation, setConversation] = useState<any>(null);
-    const [isLoading, setLoading] = useState<boolean>(false);
-    const handleCreate = async () => {
-        setConversation(null);
-        setLoading(true);
-        try {
-            const response = await fetch(routes.server.chat.createPrivateConversation, {
-                method: 'POST',
-            });
-            const { data } = await response.json();
-            setConversation(data?.createConversation);
-        } catch (e) {
-            console.error(e);
-            toast.error('Something went wrong')
-        } finally {
-            setLoading(false);
+  const [conversation, setConversation] = useState<any>(null);
+  const [isLoading, setLoading] = useState<boolean>(false);
+  const router = useRouter();
+  const handleCreate = async () => {
+    setConversation(null);
+    setLoading(true);
+    try {
+      const response = await fetch(
+        routes.server.chat.createPrivateConversation,
+        {
+          method: "POST",
         }
-    };
+      );
+      const { data } = await response.json();
+      setConversation(data?.createConversation);
+    } catch (e) {
+      console.error(e);
+      toast.error("Something went wrong");
+    } finally {
+      setLoading(false);
+    }
+  };
 
-    return (
-        <AppLayout>
-            <DemoLayout requireSession={false}>
-                <AuthLayout>
-                    <button
-                        className="btn btn-sm"
-                        onClick={handleCreate}
-                        disabled={isLoading}
-                    >
-                        Create Conversation
-                    </button>
-                    {conversation ? <pre>{JSON.stringify(conversation, null, 4)}</pre> : <></>}
-                </AuthLayout>
-            </DemoLayout>
-        </AppLayout>
-    );
-}
+  const handleRedirect = useCallback(() => {
+    if (conversation?.id) {
+      router.push(`/chat/controlled?cid=${conversation?.id}`);
+    }
+  }, [conversation?.id, router]);
 
+  return (
+    <AppLayout>
+      <DemoLayout requireSession={false}>
+        <AuthLayout>
+          <button
+            className="btn btn-sm"
+            onClick={handleCreate}
+            disabled={isLoading}
+          >
+            Create Conversation
+          </button>
+          {conversation ? (
+            <>
+              <pre>{JSON.stringify(conversation, null, 4)}</pre>
+              <button className="btn btn-sm mt-1" onClick={handleRedirect}>
+                Show Conversation
+              </button>
+            </>
+          ) : (
+            <></>
+          )}
+        </AuthLayout>
+      </DemoLayout>
+    </AppLayout>
+  );
+};
 
 export default FetchConversation;
