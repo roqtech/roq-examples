f42a30f2b5790f5dbc9f9c19d2076effc0b77d73
Jasper Bernales <bernalesjasper@gmail.com>
2023-07-27 20:03:48 +0800

    invites table locale testing

--- nextjs-with-roq-auth/src/pages/invites/table.tsx
+++ nextjs-with-roq-auth/src/pages/invites/table.tsx
@@ -1,12 +1,32 @@
 import AppLayout from '../../layout/app/app.layout';
 import DemoLayout from 'layout/demo/demo.layout';
 import { UserInvitesTable } from '@roq/nextjs'
+import { SelectFilter } from 'components/file/select.filter';
+import { useState } from 'react';
 
+const options = [
+  {
+    value: 'en-US',
+    label: 'English'
+  },
+  {
+    value: 'de-DE',
+    label: 'German'
+  }
+]
 export const InviteTable = () => {
+  const [locale, setLocale] = useState(options[0])
   return (
     <AppLayout>
       <DemoLayout>
-        <UserInvitesTable />
+        <SelectFilter
+          value={locale}
+          onChange={(val: any) => setLocale(val)}
+          options={options}
+          isClearable={true}
+          prefix="Locale:"
+        />
+        <UserInvitesTable locale={locale?.value} />
       </DemoLayout>
     </AppLayout>
   );
